737d054756bbf54134c2755a21679193f4796a81
DatOneBrainCell <crjasperdaniel@gmail.com>
2026-07-02 21:11:23 +0530

    Create HollowSquare.cs

---
+++ Patterns/HollowSquare.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Pattern {
+    
+    static void Main() {
+        
+        int n = 5;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 || i == n-1 || j == 0 || j == n-1) {
+                    Console.Write("*  ");
+                }
+                else {
+                    Console.Write("   ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+// O/P:
+// *  *  *  *  *  
+// *           *  
+// *           *  
+// *           *  
+// *  *  *  *  *  
